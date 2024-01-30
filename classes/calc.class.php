66f9b73da26429248f25ed399abdaa32d41ccc97
MrColonThree <abdullahalmonir364@gmail.com>
2024-01-30 23:51:04 +0600

    Added phpdocs and variable type in calc.class.php file

--- classes/calc.class.php
+++ classes/calc.class.php
@@ -1,41 +1,60 @@
 <?php
 
+
 class Calc
 {
-
+  /**
+   * @var string The operator to perform the calculation.
+   */
   public $operator;
+
+  /**
+   * @var int The first number for the calculation.
+   */
   public $num1;
+
+  /**
+   * @var int The second number for the calculation.
+   */
   public $num2;
 
-
+  /**
+   * Calc constructor.
+   *
+   * @param string $operator The operator to perform the calculation.
+   * @param int $num1 The first number for the calculation.
+   * @param int $num2 The second number for the calculation.
+   */
   public function __construct(string $operator, int $num1, int $num2)
   {
-
     $this->operator = $operator;
     $this->num1 = $num1;
     $this->num2 = $num2;
   }
 
+  /**
+   * Performs the calculation based on the specified operator.
+   *
+   * @return int|float The result of the calculation.
+   * @throws RuntimeException When an invalid operator is provided.
+   */
   public function calculator()
   {
     switch ($this->operator) {
       case 'add':
-        $result = $this->num1 + $this->num2;
-        return $result;
+        return $this->num1 + $this->num2;
       case 'div':
-        $result = $this->num1 / $this->num2;
-        return $result;
+        if ($this->num2 !== 0) {
+          return $this->num1 / $this->num2;
+        } else {
+          throw new RuntimeException("Cannot divide by zero.");
+        }
       case 'sub':
-        $result = $this->num1 - $this->num2;
-        return $result;
+        return $this->num1 - $this->num2;
       case 'mul':
-        $result = $this->num1 * $this->num2;
-        return $result;
-
-
+        return $this->num1 * $this->num2;
       default:
-        echo "Error";
-        break;
+        throw new RuntimeException("Invalid operator: {$this->operator}");
     }
   }
 }
